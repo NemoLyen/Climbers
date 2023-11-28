63f08768503f36e326ae9fff9442b34f753dd7b2
NemoLyen <gadzila734@gmail.com>
2023-11-28 16:30:07 +0500

    v1.1

---
+++ main.py
@@ -0,0 +1,22 @@
+from Models.Climbers import Climbers
+from Models.Model import Model
+
+# climber = Model()
+# print(climber.get('Climbers'))
+
+climber = Climbers()
+print(climber.get())
+# цикл вывода записей таблицы
+for row, list in enumerate(climber.get()):
+    print(row, ': ', list)
+
+print(climber.getOneField('name'))
+
+for row, list in enumerate (climber.getOneField('name')):
+    print(row, ') ', list)
+
+#climber.add()
+#climber.delete(3)
+
+climber.update()
+
